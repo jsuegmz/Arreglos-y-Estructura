ff72f9619df7ae0b962dc72447f2a4718f59dd2e
jsuegmz <jngomez@uamv.edu.ni>
2023-04-25 17:57:49 -0600

    Primer ejercicio de arreglos

---
+++ arreglo1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int Arreglo1() {
+   char Arreglo1[11] = {'0', '1', '5', '7', '-', 'M', 'P', '2', '0', '2', '3'};
+
+   for(int i = 0; i < 11; i++) {
+      cout << Arreglo1[i] << " ";
+   }
+   cout << endl;
+
+   return 0;
+}
