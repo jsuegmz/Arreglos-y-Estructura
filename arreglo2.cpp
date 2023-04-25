c7d1d0bb0b7e77e9f639ee63c86f8f56b34fa579
jsuegmz <jngomez@uamv.edu.ni>
2023-04-25 17:58:16 -0600

    Segundo ejericio de arreglos

---
+++ arreglo2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int Arreglo2() {
+
+    double Arreglo2[6] = {0, 95.75, 0, 87.91, 0, 85.72};
+    
+    for(int i = 0; i < 6; i++)
+    {
+      cout << Arreglo2[i] << " ";
+    }
+   
+   cout << endl;
+
+   return 0;
+}
